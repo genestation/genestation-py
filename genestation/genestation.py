74ed62d0905fcadc0077c2dbb700545689b91fbd
Mara Kim <mara.kim@vanderbilt.edu>
2018-08-30 00:45:50 -0500

    Add subprogram structure

--- genestation/genestation.py
+++ genestation/genestation.py
@@ -1,11 +1,44 @@
+#!/usr/bin/env python3
+
+import argparse
 from elasticsearch import Elasticsearch
 from schema.templates import Templates
 from module.ontology import Ontology
 
+PROG='genestation'
+VERSION="0.0.1"
+
+# Argparse
+parser = argparse.ArgumentParser(prog=PROG)
+parser.add_argument('--version', action='version',
+	version='%(prog)s '+VERSION)
+parser.add_argument('--host', action='append',
+	help='url(s) to the elasticsearch node; default localhost:9200')
+subparsers = parser.add_subparsers(
+	title='subcommands')
+
+# init command parser
+parser_init = subparsers.add_parser('init', description='initialize a new genestation instance')
+
+# load command parser
+parser_load = subparsers.add_parser('load', description='load genomic data into a genestation instance')
+parser_load.add_argument('descriptor', nargs='+', type=argparse.FileType('r'),
+	help='Genomic Data Descriptor JSON')
+
+# Parse arguments
+arg = parser.parse_args()
+if arg.host is None:
+	arg.host = ['localhost:9200']
+
+# Main
+print(arg)
+exit(0)
+
+
 def genestation():
 	pass
 class Genestation:
-	def __init__(self, hosts, genomeMetaIndex="genomes", **kwargs):
+	def __init__(self, hosts, genomeMetaIndex="genome", **kwargs):
 		self.es = ElasticSearch(hosts, **kwargs)
 		self.genomeMetaIndex = genomeMetaIndex
 	def initializeCluster(self):

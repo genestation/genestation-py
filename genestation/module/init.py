0aea6bcbbd09bc1cf5d1354e5debcd91dc4854c9
Mara Kim <mara.kim@vanderbilt.edu>
2018-09-06 04:11:50 -0500

    Add genome index config

--- genestation/module/init.py
+++ genestation/module/init.py
@@ -1,23 +1,21 @@
 from elasticsearch import Elasticsearch
+from module.schema.genome_index import GenomeIndex
 from module.schema.templates import Templates
-from module.file.obo import Ontology
 
 def main(arg):
 	print("Initializing ElasticSearch cluster")
-	print(arg)
-
-def genestation():
-	pass
-class Genestation:
-	def __init__(self, hosts, genomeMetaIndex="genome", **kwargs):
-		self.es = ElasticSearch(hosts, **kwargs)
-		self.genomeMetaIndex = genomeMetaIndex
-	def initializeCluster(self):
-		self.loadTemplates()
-	def loadTemplates(self):
-		for key, value in Templates.items():
-			self.es.put_template(id=key, body=value)
-	def loadGenome(self, genome):
-		pass
-	def parseOntology(self, genome):
-		pass
+	es = Elasticsearch(hosts=arg.host)
+	# Test connection
+	if es.ping() is False:
+		print("Cannot access ElasticSearch at", arg.host)
+		return
+	# Create genome index
+	if es.indices.exists(index="genome"):
+		print("Genome Index already exists, skipping")
+	else:
+		print("Creating Genome Index")
+		es.indices.create(index="genome", body=GenomeIndex)
+	# Create feature index template
+	# Create search templates
+	#for key, value in Templates.items():
+		#self.es.put_template(id=key, body=value)

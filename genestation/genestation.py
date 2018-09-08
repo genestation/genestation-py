6393b5c2b9f45340920c5195f2cfffd853919658
Mara Kim <mara.kim@vanderbilt.edu>
2018-09-08 01:42:01 -0500

    Add genome exploration commands

--- genestation/genestation.py
+++ genestation/genestation.py
@@ -23,9 +23,15 @@ parser_load.add_argument('descriptor', nargs='+', type=argparse.FileType('r'),
 	help='Genomic Data Descriptor JSON')
 
 # get command parser
-parser_load = subparsers.add_parser('get', description='get genomic data from a genestation instance')
-parser_load.add_argument('index', help='ElasticSearch index')
-parser_load.add_argument('id', help='Document ID')
+parser_get = subparsers.add_parser('get', description='get genomic data from a genestation instance')
+parser_get.add_argument('index', help='ElasticSearch index')
+parser_get.add_argument('id', help='Document ID')
+
+# genome command parser
+parser_genome = subparsers.add_parser('genome', description='manage genomes')
+subparsers_genome = parser_genome.add_subparsers(title='subcommands', dest='subcommand')
+parser_genome_show = subparsers_genome.add_parser('show', description='show genome')
+parser_genome_show.add_argument('genome', help='Genome Identifier')
 
 # Parse arguments
 arg = parser.parse_args()
@@ -41,4 +47,7 @@ elif arg.command == "load":
 	main(arg)
 elif arg.command == "get":
 	from module.get import main
+	main(arg)
+elif arg.command == "genome":
+	from module.genome import main
 	main(arg)
---
+++ genestation/module/genome.py
@@ -0,0 +1,38 @@
+import sys
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import scan
+
+def main(arg):
+	# ElasticSearch
+	es = Elasticsearch(arg.host,timeout=600000)
+	# Test connection
+	if es.ping() is False:
+		print("Cannot access ElasticSearch at", arg.host)
+
+	if arg.subcommand == "show":
+		show(es, arg.genome)
+	else:
+		list_genomes(es)
+
+def list_genomes(es):
+	results = scan(es, index='genome', query={
+		'query': {'match_all': {}}
+	})
+	for result in results:
+		print(result['_id'])
+
+def show(es, genome):
+	if not es.exists(index='genome', doc_type="doc", id=genome):
+		print("Cannot find genome: {0}".format(genome), file=sys.stderr)
+		return
+	resp = es.get(index='genome', doc_type="doc", id=genome)
+	print('* genome {0}'.format(resp['_id']))
+	source = resp['_source']
+	print('  Organism: {0}'.format(" ".join([source["genus"],source["species"],source["subspecies"] if source["subspecies"] else ''])))
+	print('  Version: {0}'.format(source["version"]))
+	feature_prefix = 'feature.{0}.'.format(genome)
+	resp = es.indices.get_alias(index='{0}*'.format(feature_prefix))
+	if resp:
+		print('  Features:')
+		for key in resp:
+			print('    {0}:\t{1}'.format(key[len(feature_prefix):],es.count(index=key)['count']))
